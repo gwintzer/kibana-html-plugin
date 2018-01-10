54f05c393b9b0ff13d1a23af76f97238edffe419
Guillaume Wintzer <guillaume.wintzer@orange.com>
2018-01-10 13:45:45 +0100

    v5.6.5 with monokai theme

--- public/html.js
+++ public/html.js
@@ -1,41 +1,34 @@
+
+import 'plugins/kibana-html-plugin/deps/ace-builds/ace'
+import 'plugins/kibana-html-plugin/deps/ace-builds/mode-html'
+import 'plugins/kibana-html-plugin/deps/ace-builds/theme-monokai'
+import 'plugins/kibana-html-plugin/deps/angular-ui-ace/ui-ace.min'
+
 import 'plugins/kibana-html-plugin/html.less';
+import 'plugins/kibana-html-plugin/htmlController';
+import { VisVisTypeProvider } from 'ui/vis/vis_type';
+import { TemplateVisTypeProvider } from 'ui/template_vis_type/template_vis_type';
+import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
 
-import mainTemplate from 'plugins/kibana-html-plugin/html.html';
-import optionsTemplate from 'plugins/kibana-html-plugin/htmlOptions.html';
-
-import 'plugins/kibana-html-plugin/htmlController.js';
-
-import {CATEGORY} from 'ui/vis/vis_category';
-import {VisFactoryProvider} from 'ui/vis/vis_factory';
-import {VisTypesRegistryProvider} from 'ui/registry/vis_types';
-import {VisSchemasProvider} from 'ui/vis/editors/default/schemas';
+VisTypesRegistryProvider.register(HtmlVisProvider);
 
 function HtmlVisProvider(Private) {
-  const VisFactory = Private(VisFactoryProvider);
-  const Schemas = Private(VisSchemasProvider);
+  const VisType = Private(VisVisTypeProvider);
+  const TemplateVisType = Private(TemplateVisTypeProvider);
 
-  return VisFactory.createAngularVisualization({
-      name: 'html',
-      title: 'Html widget',
-      icon: 'fa-code',
-      description: 'Useful for displaying html in dashboards.',
-      category: CATEGORY.OTHER,
-    //visualization: VisController,
-
-    visConfig: {
-      defaults: {},
-      template: mainTemplate
+  return new TemplateVisType({
+    name: 'html',
+    title: 'Html widget',
+    icon: 'fa-code',
+    description: 'Useful for displaying html in dashboards.',
+    category: VisType.CATEGORY.OTHER,
+    template: require('plugins/kibana-html-plugin/html.html'),
+    params: {
+      editor: require('plugins/kibana-html-plugin/htmlOptions.html')
     },
-    editorConfig: {
-      optionsTemplate: optionsTemplate,
-      schemas: new Schemas([{
-        group: 'metrics',
-        name: 'metric',
-        title: 'Metric',
-        min: 1,
-        aggFilter: ['!derivative', '!geo_centroid']
-      }]),
-    }
+    requiresSearch: false
   });
 }
-VisTypesRegistryProvider.register(HtmlVisProvider);
+
+export default HtmlVisProvider;
+
