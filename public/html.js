35e02d7dac6635b999aeefb014ed8907ac8824df
Guillaume Wintzer <guillaume.wintzer@orange.com>
2020-02-04 16:13:06 +0100

    update to version 7.5.2 (#18)
    update to version 7.5.2

--- public/html.js
+++ public/html.js
@@ -1,44 +1,40 @@
 
-import {VisFactoryProvider} from 'ui/vis/vis_factory';
-import {VisTypesRegistryProvider} from 'ui/registry/vis_types';
+import { visFactory } from 'ui/vis/vis_factory';
+import { setup, start } from '../../../src/legacy/core_plugins/visualizations/public/np_ready/public/legacy';
 
 import { DefaultEditorSize } from 'ui/vis/editor_size';
 
 import { HtmlVisWrapper } from './html_vis_controller';
 import htmlVisParamsTemplate from './html_vis_params.html';
 
+    
+const htmlVis = () => visFactory.createReactVisualization({
+  name: 'html',
+  title: 'Html widget',
+  icon: 'editorCodeBlock',
+  description: 'Useful for displaying html in dashboards.',
 
-function HtmlVisProvider(Private) {
-  const VisFactory = Private(VisFactoryProvider);
+  visConfig: {
+    component: HtmlVisWrapper,
+    defaults: {
+      fontSize: 12,
+      openLinksInNewTab: false
+    }
+  },
+  editorConfig: {
+    optionsTemplate: htmlVisParamsTemplate,
+    enableAutoApply: true,
+    defaultSize: DefaultEditorSize.LARGE,
+  },
+  options: {
+    showTimePicker: false,
+  },
+  requestHandler: 'none',
+  responseHandler: 'none',
+});
 
-  return VisFactory.createReactVisualization({
-    name: 'html',
-    title: 'Html widget',
-    icon: 'editorCodeBlock',
-    description: 'Useful for displaying html in dashboards.',
+setup.types.registerVisualization(htmlVis);
 
-    visConfig: {
-      component: HtmlVisWrapper,
-      defaults: {
-        fontSize: 12,
-        openLinksInNewTab: false
-      }
-    },
-    editorConfig: {
-      optionsTemplate: htmlVisParamsTemplate,
-      enableAutoApply: true,
-      defaultSize: DefaultEditorSize.LARGE,
-    },
-    options: {
-      showTimePicker: false,
-    },
-    requestHandler: 'none',
-    responseHandler: 'none',
-  });
-}
-
-// register the provider with the visTypes registry so that other know it exists
-VisTypesRegistryProvider.register(HtmlVisProvider);
 
 // export the provider so that the visType can be required with Private()
-export default HtmlVisProvider;
+export default htmlVis;
